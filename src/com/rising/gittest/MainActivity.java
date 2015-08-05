95ad8d57af6a0cb01450887eb8fbd68c2a8cfca6
ZL <1193018305@qq.com>
2015-08-05 09:02:53 +0800

    commit

--- src/com/rising/gittest/MainActivity.java
+++ src/com/rising/gittest/MainActivity.java
@@ -1,17 +1,21 @@
 package com.rising.gittest;
 
+import com.rising.utils.Utils;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private TextView mTextDpi;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mTextDpi = (TextView) findViewById(R.id.textdpi);
+		mTextDpi.setText(Utils.getDisplayMetrics(MainActivity.this));
 	}
-
-	
 
 }
--- src/com/rising/utils/Utils.java
+++ src/com/rising/utils/Utils.java
@@ -1,12 +1,19 @@
 package com.rising.utils;
 
 import android.content.Context;
+import android.util.Log;
 
 public class Utils {
-	
-	public static int dip2px(Context context, float dpValue) {  
-        final float scale = context.getResources().getDisplayMetrics().density;  
-        return (int) (dpValue * scale + 0.5f);  
-    } 
+
+	public static int dip2px(Context context, float dpValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dpValue * scale + 0.5f);
+	}
+
+	public static String getDisplayMetrics(Context context) {
+		float xdpi = context.getResources().getDisplayMetrics().xdpi;
+		float ydpi = context.getResources().getDisplayMetrics().ydpi;
+		return "xdpi : " + xdpi + ", ydpi : " + ydpi;
+	}
 
 }
